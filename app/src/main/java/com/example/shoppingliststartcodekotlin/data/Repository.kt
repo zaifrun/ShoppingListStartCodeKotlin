ff959de4e60bdd2a78edeab4fe6e2bd8f38933b6
zaifrun <martinknud@gmail.com>
2021-03-23 14:42:49 +0100

    added productlistener

--- app/src/main/java/com/example/shoppingliststartcodekotlin/data/Repository.kt
+++ app/src/main/java/com/example/shoppingliststartcodekotlin/data/Repository.kt
@@ -1,6 +1,17 @@
 package com.example.shoppingliststartcodekotlin.data
 
+import androidx.lifecycle.MutableLiveData
+
 object Repository {
     var products = mutableListOf<Product>()
+
+    private var productListener = MutableLiveData<MutableList<Product>>()
+
+
+    fun getData(): MutableLiveData<MutableList<Product>> {
+        //createTestData()
+        productListener.value = products
+        return productListener
+    }
 
 }
