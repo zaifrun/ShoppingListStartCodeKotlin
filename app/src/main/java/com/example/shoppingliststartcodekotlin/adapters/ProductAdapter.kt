39eefa25de23676290a4c415f45aa7804edaf788
zaifrun <martinknud@gmail.com>
2021-04-08 09:50:15 +0200

    Added more comments and changed a little in the Repository class

--- app/src/main/java/com/example/shoppingliststartcodekotlin/adapters/ProductAdapter.kt
+++ app/src/main/java/com/example/shoppingliststartcodekotlin/adapters/ProductAdapter.kt
@@ -23,7 +23,7 @@ class ProductAdapter(var products: MutableList<Product>) :
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         //here you need to to do stuff also - to back to the exercises
         //about recyclerviews and you can use approach that were used
-        //in the exercise about recyclerviews fro the book (lesson 3)
+        //in the exercise about recyclerviews from the book (lesson 3)
         //if you did not do that exercise - then first do that exercise in
         //a seperate project
 
--- app/src/main/java/com/example/shoppingliststartcodekotlin/data/Repository.kt
+++ app/src/main/java/com/example/shoppingliststartcodekotlin/data/Repository.kt
@@ -5,18 +5,20 @@ import androidx.lifecycle.MutableLiveData
 object Repository {
     var products = mutableListOf<Product>()
 
+    //listener to changes that we can then use in the Activity
     private var productListener = MutableLiveData<MutableList<Product>>()
 
 
     fun getData(): MutableLiveData<MutableList<Product>> {
-        //createTestData()
+        if (products.isEmpty())
+            createTestData()
         productListener.value = products
         return productListener
     }
 
     fun createTestData()
     {
-        //add some products to the products list
+        //add some products to the products list - for testing purposes
     }
 
 }
